636e4cff6301976aaffb1c660052ba50a572545f
Marcio Portes de Albuquerque <marcioportesdealbuquerque@Marcios-MacBook-Pro.local>
2022-04-07 00:07:22 -0300

    Dividindo, removendo, adicionando e atualizando Arrays

---
+++ dividindo-arrays.js
@@ -0,0 +1,10 @@
+const nomes = ['joao', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 
+'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 
+'Renan', 'Renata', 'Daisy', 'Camilo'];
+
+const sala1 = nomes.slice(0, nomes.length / 2);
+const sala2 = nomes.slice(nomes.length / 2);
+
+console.log(nomes.length);
+console.log(`Alunos da Sala 1 ${sala1}`);
+console.log(`Alunos da Sala 2 ${sala2}`);
